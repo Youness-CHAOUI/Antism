b2b1f2e56e2d9ab0d1e3f956804811f267ea3482
youness_chaoui <younesschaoui15@gmail.com>
2019-04-17 03:43:23 +0100

    home screen & start screen

--- app/src/main/java/com/injaz2019/antism/MainActivity.java
+++ app/src/main/java/com/injaz2019/antism/MainActivity.java
@@ -1,13 +1,30 @@
 package com.injaz2019.antism;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    /*Variables*/
+    Button btn_start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_start = findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v)
+    {
+        startActivity(new Intent(this, StartActivity.class));
+        this.finish();
+    }
+
 }
