1d591294be3829bc99bcb7c2a37972a6dedfecd5
youness_chaoui <younesschaoui15@gmail.com>
2019-04-18 02:58:18 +0100

    child & assistant + assistant spaces

--- app/src/main/java/com/injaz2019/antism/StartActivity.java
+++ app/src/main/java/com/injaz2019/antism/StartActivity.java
@@ -1,13 +1,53 @@
 package com.injaz2019.antism;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class StartActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity implements View.OnClickListener {
+
+    /*Variables*/
+    TextView tv_ca, tv_a;
+    View v_ca, v_a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        tv_ca = findViewById(R.id.tv_childAssistant);
+        tv_a = findViewById(R.id.tv_assitant);
+        v_ca = findViewById(R.id.v_childAssistant);
+        v_a = findViewById(R.id.v_assitant);
+
+        tv_ca.setOnClickListener(this);
+        tv_a.setOnClickListener(this);
+        v_ca.setOnClickListener(this);
+        v_a.setOnClickListener(this);
+    }
+
+
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId()) {
+            case R.id.tv_childAssistant:
+                startActivity(new Intent(this, childAssitantActivity.class));
+                break;
+            case R.id.v_childAssistant:
+                startActivity(new Intent(this, childAssitantActivity.class));
+                break;
+            case R.id.tv_assitant:
+                startActivity(new Intent(this, AssitantActivity.class));
+                break;
+            case R.id.v_assitant:
+                startActivity(new Intent(this, AssitantActivity.class));
+                break;
+
+        }
     }
 }
---
+++ app/src/main/java/com/injaz2019/antism/childAssitantActivity.java
@@ -0,0 +1,61 @@
+package com.injaz2019.antism;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class childAssitantActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //    /*Variables*/
+    //    TextView tv_ca, tv_a;
+    //    View v_ca, v_a;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child_assitant);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_start);
+//
+//        tv_ca = findViewById(R.id.tv_childAssistant);
+//        tv_a = findViewById(R.id.tv_assitant);
+//        v_ca = findViewById(R.id.v_childAssistant);
+//        v_a = findViewById(R.id.v_assitant);
+//
+//        tv_ca.setOnClickListener(this);
+//        tv_a.setOnClickListener(this);
+//        v_ca.setOnClickListener(this);
+//        v_a.setOnClickListener(this);
+//    }
+//
+//
+//
+//    @Override
+//    public void onClick(View v)
+//    {
+//        switch (v.getId()) {
+//            case R.id.tv_childAssistant:
+//                startActivity(new Intent(this, childAssitantActivity.class));
+//                break;
+//            case R.id.v_childAssistant:
+//                startActivity(new Intent(this, childAssitantActivity.class));
+//                break;
+//            case R.id.tv_assitant:
+//                startActivity(new Intent(this, AssitantActivity.class));
+//                break;
+//            case R.id.v_assitant:
+//                startActivity(new Intent(this, AssitantActivity.class));
+//                break;
+//        }
+//    }
+}
